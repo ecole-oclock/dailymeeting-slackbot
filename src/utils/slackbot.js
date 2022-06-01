cd775fdddc9aeab90f97ccf4c64e197c3f06031e
Sébastien Lampazona <sebastien@oclock.io>
2022-06-01 13:31:10 +0200

    refactor(env): suppression de variables inutiles

--- src/utils/slackbot.js
+++ src/utils/slackbot.js
@@ -10,7 +10,6 @@ dayjs.extend(localizedFormat);
 dayjs.extend(timezone);
 dayjs.tz.setDefault(process.env.TZ);
 // An access token (from your Slack app or custom integration - xoxp, xoxb)
-// const token = process.env.SLACK_USER_TOKEN; // Add a bot https://my.slack.com/services/new/bot and put the token
 const token = process.env.SLACK_BOT_TOKEN; // Add a bot https://my.slack.com/services/new/bot and put the token
 
 export default new WebClient(token);
